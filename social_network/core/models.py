d3a0429e5ce19b56f983f3746db534d3e59c5712
antasprojects <140523786+antasprojects@users.noreply.github.com>
2023-12-07 22:37:53 +0000

    setting the server

--- social_network/core/models.py
+++ social_network/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profileimg = models.ImageField()
+    location = pass
+
--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
